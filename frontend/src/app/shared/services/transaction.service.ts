7fbf97a137459a4ff3732ea2355ce48322cf6293
Tiago Barbosa <sombr43@gmail.com>
2024-12-29 16:14:31 -0300

    melhorias de colocar em pastas

--- frontend/src/app/shared/services/transaction.service.ts
+++ frontend/src/app/shared/services/transaction.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Category, Transaction } from './transaction.model';
+import { Category, Transaction } from '../models/transaction.model';
 
 @Injectable({
   providedIn: 'root'
@@ -9,18 +9,18 @@ import { Category, Transaction } from './transaction.model';
 export class TransactionService {
 
   transactionsApi = 'http://localhost:3001/transactions';
-  categoryApi =  'http://localhost:3001/categories';
+  categoryApi = 'http://localhost:3001/categories';
 
   transactions: Transaction[] = [];
   categories: Category[] = [];
 
   constructor(private http: HttpClient) { }
 
-  getTransactions(): Observable<Transaction[]>{
-    return this.http.get<Transaction[]>(this.transactionsApi); 
+  getTransactions(): Observable<Transaction[]> {
+    return this.http.get<Transaction[]>(this.transactionsApi);
   }
 
-  getCategories(): Observable<Category[]>{
+  getCategories(): Observable<Category[]> {
     return this.http.get<Category[]>(this.categoryApi)
   }
 }
