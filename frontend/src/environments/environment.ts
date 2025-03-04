1ece41ae38fb7dddb5be384e0b8ee9d79703bff9
Tiago Barbosa <sombr43@gmail.com>
2025-03-03 23:05:37 -0300

    fix: Correção de deploy de api

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  transactionUrl: 'http://localhost:8080/transactions',
-  categoryUrl: 'http://localhost:8080/categories',
+  transactionUrl: 'https://backend-finances-ms-transactions.onrender.com/transactions',
+  categoryUrl: 'https://backend-finances-ms-transactions.onrender.com/categories',
 };
